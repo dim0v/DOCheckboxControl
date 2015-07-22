7203151bdc03d7ad69b134a05b4bfb622a224ef4
Dmytro Ovcharenko <dmyt.ov@gmail.com>
2015-07-23 01:05:30 +0300

    demo update

--- Example/DOCheckboxControl/MainTableViewController.swift
+++ Example/DOCheckboxControl/MainTableViewController.swift
@@ -7,7 +7,27 @@
 //
 
 import UIKit
+import DOCheckboxControl
 
 class MainTableViewController: UITableViewController {
 
+    @IBOutlet weak var programmaticalySwitchableCheckbox: CheckboxControl!
+    
+    override func viewDidLoad() {
+        let switchAnimatedButton = UIBarButtonItem(title: "Switch animated", style: UIBarButtonItemStyle.Plain, target: self, action: "switchAnimated")
+        let switchNotAnimatedButton = UIBarButtonItem(title: "Switch instantly", style: UIBarButtonItemStyle.Plain, target: self, action: "switchNotAnimated")
+        
+        self.navigationItem.leftBarButtonItem = switchAnimatedButton
+        self.navigationItem.rightBarButtonItem = switchNotAnimatedButton
+        
+        programmaticalySwitchableCheckbox.enabled = false
+    }
+    
+    dynamic func switchAnimated() {
+        programmaticalySwitchableCheckbox.setSelected(!programmaticalySwitchableCheckbox.selected, animated: true)
+    }
+    
+    dynamic func switchNotAnimated() {
+        programmaticalySwitchableCheckbox.setSelected(!programmaticalySwitchableCheckbox.selected, animated: false)
+    }
 }
